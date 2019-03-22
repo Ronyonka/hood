97e2df95636c39f9337e731954371c93fff4610e
Ronyonka <rononyonka@gmail.com>
2019-03-22 14:14:08 +0300

    added models for profile business and posts

--- hood/models.py
+++ hood/models.py
@@ -11,4 +11,20 @@ class Neighbourhood(models.Model):
     location = models.ForeignKey(Location)
 
 class Profile(models.Model):
-    user = models.OneToOneField(User,)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(null=True,blank=True, upload_to='avatars/')
+    bio = models.TextField(null=True,blank=True,max_length=500)
+    hood = models.ForeignKey(Neighbourhood, related_name='prohood')
+    location = models.ForeignKey(Location, related_name='prolocation')
+
+class Business(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField(blank=True,null=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    hood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+
+class Posts(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField(max_length=500)
+    author = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    hood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
