0d92dd6530b52c2307de06e15080f34c2f0399c7
Ronyonka <rononyonka@gmail.com>
2019-03-22 14:37:02 +0300

    added models for contacts and comments

--- hood/models.py
+++ hood/models.py
@@ -10,12 +10,23 @@ class Neighbourhood(models.Model):
     name = models.CharField(max_length= 140)
     location = models.ForeignKey(Location)
 
+class Contact(models.Model):
+    health = models.CharField(max_length = 30)
+    police = models.CharField(max_length=30)
+    location = models.ForeignKey(Location, related_name='emergency')
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(null=True,blank=True, upload_to='avatars/')
     bio = models.TextField(null=True,blank=True,max_length=500)
     hood = models.ForeignKey(Neighbourhood, related_name='prohood')
     location = models.ForeignKey(Location, related_name='prolocation')
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
 
 class Business(models.Model):
     name = models.CharField(max_length=80)
@@ -28,3 +39,9 @@ class Posts(models.Model):
     post = models.TextField(max_length=500)
     author = models.ForeignKey(Profile,on_delete=models.CASCADE)
     hood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+
+class Comments(models.Model):
+    comment = models.TextField(max_length=280)
+    created = models.DateTimeField(auto_now_add = True,null = True)
+    author = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts,related_name='post')
