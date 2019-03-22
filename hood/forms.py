3ecccef1074a513d1cf42fe1d3e93185890aacfe
Ronyonka <rononyonka@gmail.com>
2019-03-22 19:57:51 +0300

    added signup form

--- hood/forms.py
+++ hood/forms.py
@@ -1,2 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+from .models import *
+
+class SignUpForm(UserCreationForm):
+    '''
+    Form that extends the UserCreationForm
+    Added fields are the email fields
+    '''
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    email= forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}), max_length=64)
+    password1= forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+
+
+    class Meta:
+        model = User
+        fields = UserCreationForm.Meta.fields + ("email","username","password1")
