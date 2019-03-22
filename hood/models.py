e93adb5f46e418fde4e1ad28d7185355b1854d5d
Ronyonka <rononyonka@gmail.com>
2019-03-22 13:48:35 +0300

    added models for location and neighbourhood

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=140)
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length= 140)
+    location = models.ForeignKey(Location)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,)
